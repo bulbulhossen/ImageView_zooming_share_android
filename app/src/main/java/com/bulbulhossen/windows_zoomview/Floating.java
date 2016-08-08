2ec309d02eeb2050f51696708b949250c90d749f
bulbulhossen <info247.bulbul@gmail.com>
2016-08-09 01:31:53 +0600

    Initial commit

---
+++ app/src/main/java/com/bulbulhossen/windows_zoomview/Floating.java
@@ -0,0 +1,51 @@
+package com.bulbulhossen.windows_zoomview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.github.fafaldo.fabtoolbar.widget.FABToolbarLayout;
+
+
+public class Floating extends AppCompatActivity implements View.OnClickListener{
+    private FABToolbarLayout layout;
+    private View one, two, three, four;
+    private View fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.floating_toolbar);
+
+        layout = (FABToolbarLayout) findViewById(R.id.fabtoolbar);
+        one = findViewById(R.id.one);
+        two = findViewById(R.id.two);
+        three = findViewById(R.id.three);
+        four = findViewById(R.id.four);
+        fab = findViewById(R.id.fabtoolbar_fab);
+
+        one.setOnClickListener(this);
+        two.setOnClickListener(this);
+        three.setOnClickListener(this);
+        four.setOnClickListener(this);
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.show();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        layout.hide();
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "Element clicked", Toast.LENGTH_SHORT).show();
+    }
+}
